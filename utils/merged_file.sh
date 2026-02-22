911859b17bdbbcb8f2d15caa22581faae9fff5a1
samuelburger27 <samuel.burger027@gmail.com>
2026-02-22 20:59:20 +0100

    Reverse the files in merge generator

--- utils/merged_file.sh
+++ utils/merged_file.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 printf "" > merged.rs
-find src -name "*.rs" -print0 | while IFS= read -r -d '' file; do
+find src -name "*.rs" -print0 | sort -rz | while IFS= read -r -d '' file; do
     echo "// FILE: $file" >> merged.rs
     cat "$file" >> merged.rs
     echo >> merged.rs
