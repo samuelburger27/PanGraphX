b99b59e0e80d26d65176fc54c33101a5bc1e8387
samuelburger27 <samuel.burger027@gmail.com>
2026-03-12 16:18:34 +0100

    Chnage gbz file codec

--- pangraphx-core/tests/test_gbz.rs
+++ pangraphx-core/tests/test_gbz.rs
@@ -1,43 +1,44 @@
 use pangraphx_core::*;
-use std::fs;
+use std::{fs, path};
 use tempfile::NamedTempFile;
 
-#[ignore = "GBZ format test is not implemented yet."]
 #[test]
 fn test_gbz_format() {
-    println!("GBZ format test is not implemented yet.");
-    assert!(true);
-    // // TODO finish this , it is
-    // // Test all GFA files in the test_files/gfa directory
-    // // Read each file, parse it, serialize it back, and compare the contents
-    // let format = GraphFormat::GBZ;
-    // let test_files = fs::read_dir("tests/test_files/gfa").unwrap();
-    // for entry in test_files {
-    //     let path = entry.unwrap().path();
-    //     let path_string = path.to_str().unwrap();
-    //     let graph = CoreGraphDTO::load_from_file(path_string, format);
-    //     assert!(graph.is_ok(), "Failed to parse GFA file: {}", path_string);
-    //     let graph = graph.unwrap();
-    //     let temp_file = NamedTempFile::new().unwrap();
-    //     graph
-    //         .save_to_file(temp_file.path().to_str().unwrap(), format)
-    //         .unwrap();
-    //     assert!(temp_file.path().exists());
+    let format = GraphFormat::GBZ;
+    let test_files = fs::read_dir("tests/test_files/gbz").unwrap();
+    for entry in test_files {
+        let path = entry.unwrap().path();
+        let path_string = path.to_str().unwrap();
+        println!("Testing GBZ file: {}... ", path_string);
+        let graph = CoreGraphDTO::load_from_file(path_string, format).unwrap();
+        //assert!(graph.is_ok(), "Failed to parse GBZ file: {}", path_string);
+        //let graph = graph.unwrap();
+        let temp_file = NamedTempFile::new().unwrap();
+        // Save to GFA fromat, since GBZ serialization is currently not supported
 
-    //     let reloaded_graph =
-    //         CoreGraphDTO::load_from_file(temp_file.path().to_str().unwrap(), format).unwrap();
-    //     assert_eq!(
-    //         graph, reloaded_graph,
-    //         "Graphs do not match after save/load cycle for file: {}",
-    //         path_string
-    //     );
+        graph
+            .save_to_file(temp_file.path().to_str().unwrap(), GraphFormat::GFA)
+            .unwrap();
+        assert!(temp_file.path().exists());
 
-    //     // Compare file contents
-    //     let original_content = fs::read_to_string(path_string).unwrap();
-    //     let saved_content = fs::read_to_string(temp_file.path()).unwrap();
-    //     assert_eq!(
-    //         original_content, saved_content,
-    //         "File contents do not match after save/load cycle for file: {}",
-    //         path_string
-    //     );
+        let reloaded_graph =
+            CoreGraphDTO::load_from_file(temp_file.path().to_str().unwrap(), GraphFormat::GFA)
+                .unwrap();
+
+        assert_eq!(
+            graph.nodes, reloaded_graph.nodes,
+            "Graphs do not match after save/load cycle for file: {}",
+            path_string
+        );
+        assert_eq!(
+            graph.edges, reloaded_graph.edges,
+            "Graphs do not match after save/load cycle for file: {}",
+            path_string
+        );
+        // assert_eq!(
+        //     graph.paths, reloaded_graph.paths,
+        //     "Graphs do not match after save/load cycle for file: {}",
+        //     path_string
+        // );
+    }
 }
